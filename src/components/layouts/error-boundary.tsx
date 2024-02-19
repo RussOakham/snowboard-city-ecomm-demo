47d262bd5a7bf9787d51907b967eef5cfa48c316
Russell Oakham <russell.oakham@and.digital>
2024-02-19 16:49:48 +0000

    Renamed error function

--- src/components/layouts/error-boundary.tsx
+++ src/components/layouts/error-boundary.tsx
@@ -5,7 +5,7 @@ import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary'
 import Error from '@/app/(home)/_components/Error'
 
 interface MyErrorInfo {
-	componentStack?: string | null | undefined
+	componentStack?: string | null
 }
 
 interface ErrorBoundaryProps {
